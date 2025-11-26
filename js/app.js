33cfa1ed6d36f1b42ed5a789c3be48d489a51953
Michelle Cheng <cheng.michelley@gmail.com>
2025-11-25 21:22:36 -0500

    Exercise 12

--- js/app.js
+++ js/app.js
@@ -243,3 +243,18 @@ const createUsername = (firstNamee, secondNamee)=> {
 }
 
 console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs() {
+    return(arguments.length);
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
